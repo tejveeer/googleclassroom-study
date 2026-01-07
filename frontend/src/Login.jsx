fb4c30e7be3117412c4e620f00518c73360b2a4d
Tejveer Rehsi <tejveerrehsi@gmail.com>
2026-01-07 16:44:16 -0500

    Implemented login page

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const API_BASE = "http://localhost:3000";
+
+export function Login() {
+  const handleLogin = async () => {
+    window.location.href = `${API_BASE}/api/auth/google`;
+  }
+
+  return (
+    <div className="min-h-screen mx-auto max-w-max flex flex-col gap-2 justify-center items-center select-none">
+      <h1 className="text-3xl font-bold">Google Classroom</h1>
+      <button className="
+        text-xl cursor-pointer px-6 py-1 self-end
+        rounded-lg hover:rounded-xl transition-[border-radius] 
+        ease-out duration-200 bg-gray-400"
+        onClick={handleLogin}
+      >
+          Login
+      </button>
+    </div>
+  )
+}
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import '../index.css'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { Login } from './Login'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 )
